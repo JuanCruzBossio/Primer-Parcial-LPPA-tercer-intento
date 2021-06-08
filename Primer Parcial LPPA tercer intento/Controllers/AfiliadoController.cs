901ad8e301aab0f502a71e5334a97b700fcd3e97
Juan Cruz Bossio <JuanCruz.Bossio@Alumnos.uai.edu.ar>
2021-06-08 12:49:14 -0300

    Agregamos CSS, Layout y Logo

---
+++ Primer Parcial LPPA tercer intento/Controllers/AfiliadoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Primer_Parcial_LPPA_tercer_intento.Controllers
+{
+    public class AfiliadoController : Controller
+    {
+        // GET: Afiliado
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
